146b16ad599b11fa81a336c94cc6360dc09f19bb
Juliane <juliane.correia@prof.unipar.br>
2024-02-24 10:00:06 -0300

    first commit

---
+++ src/main/java/br/com/unipar/frameworks/jogoadivinhacao/JogoAdivinhacao.java
@@ -0,0 +1,8 @@
+package br.com.unipar.frameworks.jogoadivinhacao;
+
+public class JogoAdivinhacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
